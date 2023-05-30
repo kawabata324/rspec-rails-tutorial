a67ef410fef7f128faeeca6a0c64100aa2d09028
kawabata324 <kawabataharuki48@gmail.com>
2023-05-30 21:13:22 +0900

    :white_check_mark: test add

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+  describe "#home" do
+    it "レスポンスが正しいこと" do
+      get :home
+      expect(response).to be_successful
+      expect(response.status).to eq 200
+    end
+  end
+  describe "#help" do
+    it do
+      get :help
+      expect(response).to be_successful
+      expect(response.status).to eq 200
+    end
+  end
+end
