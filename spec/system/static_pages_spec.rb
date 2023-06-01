9d25736497e9db3698af40fa7875e17d8c0c40d4
kawabata324 <kawabataharuki48@gmail.com>
2023-06-01 23:55:56 +0900

    :sparkles: helperの追加

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -4,32 +4,32 @@ RSpec.describe "StaticPages", type: :system do
   before do
     driven_by(:rack_test)
   end
+  before do
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "#home" do
-    before do
-      @base_title = "Ruby on Rails Tutorial Sample App"
-    end
     scenario "タイトルが正しいこと" do
       visit "/static_pages/home"
-      expect(page).to have_title "Home | #{@base_title}"
+      expect(page.title).to eq "#{@base_title}"
     end
   end
   describe "#help" do
     scenario "タイトルが正しいこと" do
       visit "/static_pages/help"
-      expect(page).to have_title "Help | #{@base_title}"
+      expect(page.title).to eq "Help | #{@base_title}"
     end
   end
   describe "#about" do
     scenario "タイトルが正しいこと" do
       visit "/static_pages/about"
-      expect(page).to have_title "About | #{@base_title}"
+      expect(page.title).to eq "About | #{@base_title}"
     end
   end
   describe "#contact" do
     scenario "タイトルが正しいこと" do
       visit "/static_pages/contact"
-      expect(page).to have_title "Contact | #{@base_title}"
+      expect(page.title).to eq "Contact | #{@base_title}"
     end
   end
 end
